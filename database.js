c329467de96e59cfae1953ad3aee685759d32a10
hara <stefan.harabagiu2000@gmail.com>
2023-04-16 17:51:29 +0300

    -differentiated between development mode and release mode
    Signed-off-by: hara <stefan.harabagiu2000@gmail.com>

--- database.js
+++ database.js
@@ -1,4 +1,3 @@
-const {Client} = require('pg');
 const {Pool} = require('pg');
 
 const config = {
@@ -16,34 +15,36 @@ const config = {
 const pool = new Pool(config);
 let connectionCount = 0;
 let acquireCount = 0;
-pool.on("end", () => console.log("Pool closed!"))
-pool.on("error", (e, client) => {
-    console.error("Db client encountered errors!", e.message, e.stack);
-    client.release();
-});
-pool.on("notice", msg => console.warn("Pool notice", msg));
-pool.on("connect", client => {
-    connectionCount++;
-    console.log("Client connected!");
-});
-pool.on("acquire", client => {
-    acquireCount++;
-    console.log("Client acquired!");
-});
 
-pool.connect().then(client => {
-    client.query("SELECT NOW()").then(rows => {
-        console.log(`Pinged successfully! ${rows.rows[0]['now']}`)
-    }).catch(e => {
-        client.release();
-        console.log("Ping error!", e.message, e.stack);
-    });
-}).catch(e => {
-    pool.end();
-    console.error("Db connection error!", e.message, e.stack);
-});
 
 if (process.env.NODE_ENV === "development") {
+    pool.on("end", () => console.log("Pool closed!"))
+    pool.on("error", (e, client) => {
+        console.error("Db client encountered errors!", e.message, e.stack);
+        client.release();
+    });
+    pool.on("notice", msg => console.warn("Pool notice", msg));
+    pool.on("connect", client => {
+        connectionCount++;
+        console.log("Client connected!");
+    });
+    pool.on("acquire", client => {
+        acquireCount++;
+        console.log("Client acquired!");
+    });
+
+    pool.connect().then(client => {
+        client.query("SELECT NOW()").then(rows => {
+            console.log(`Pinged successfully! ${rows.rows[0]['now']}`);
+        }).catch(e => {
+            client.release();
+            console.log("Ping error!", e.message, e.stack);
+        });
+    }).catch(e => {
+        pool.end();
+        console.error("Db connection error!", e.message, e.stack);
+    });
+
     module.exports = {
         async query(text, params) {
             const start = Date.now()
@@ -55,6 +56,27 @@ if (process.env.NODE_ENV === "development") {
     };
 }
 else {
+    pool.on("end", () => console.log("Pool closed!"))
+    pool.on("error", (e, client) => {
+        client.release();
+    });
+    pool.on("notice", msg => console.warn("Pool notice", msg));
+    pool.on("connect", client => {
+        connectionCount++;
+    });
+    pool.on("acquire", client => {
+        acquireCount++;
+    });
+
+    pool.connect().then(client => {
+        client.query("SELECT NOW()").then(rows => {
+        }).catch(e => {
+            client.release();
+        });
+    }).catch(e => {
+        pool.end();
+    });
+
     module.exports = {
         async query(text, params) {
             return pool.query(text, params)
