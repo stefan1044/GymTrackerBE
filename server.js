4935b3685f18ea5f254026a548942dd7d1f79d58
hara <stefan.harabagiu2000@gmail.com>
2023-04-29 21:30:39 +0300

    -changed project startup to better accommodate tests
    Signed-off-by: hara <stefan.harabagiu2000@gmail.com>

---
+++ server.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const pool = require('./database');
+const logger = require('./utils/logger');
+
+const runServer = ()=> {
+
+    const middlewares = require('./middlewares/middlewares');
+    middlewares(app);
+
+    const mountRoutes = require('./routes/api');
+    mountRoutes(app);
+
+
+    const port = process.env.PORT;
+    logger.log({
+        level: "info",
+        message: `Node environment: ${process.env.NODE_ENV}`
+    });
+    const server = app.listen(port, () => {
+        logger.log({
+            level: "info",
+            message: `App listening at port http://localhost:${port}`
+        });
+    });
+
+    // TODO: Perform better cleanup
+    process.on("SIGTERM", () => {
+        logger.info(`Received SIGTERM`);
+        if (server) {
+            server.close();
+            pool.end();
+        }
+    });
+    process.on("SIGINT", () => {
+        logger.info(` Received SIGINT`);
+        if (server) {
+            server.close();
+            pool.end();
+        }
+    });
+};
+
+const closeServer = ()=>{
+    process.kill(process.pid, "SIGTERM");
+};
+
+
+module.exports = {
+    runServer, closeServer
+};
